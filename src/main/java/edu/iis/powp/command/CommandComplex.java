fc82748cb8ddd55c0e7d51307343bf2398a3e7c1
xartez <jakubchludzinski@gmail.com>
2017-04-27 15:12:44 +0200

    implementacja ComplexCommand

--- src/main/java/edu/iis/powp/command/CommandComplex.java
+++ src/main/java/edu/iis/powp/command/CommandComplex.java
@@ -1,5 +1,33 @@
 package edu.iis.powp.command;
 
-public class CommandComplex {
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class CommandComplex implements PlotterCommand {
+	private List<PlotterCommand> queue = new ArrayList<>();
+	
+	public CommandComplex(List<PlotterCommand> queue) {
+		super();
+		this.queue = queue;
+	}
+
+
+	@Override
+	public void execute(IPlotter driver) {
+		for (Object command : queue) {
+            ((PlotterCommand)command).execute(driver);
+        }
+	}
+	
+	
+	public List<PlotterCommand> getQueue() {
+		return queue;
+	}
+
+	public void setQueue(List<PlotterCommand> queue) {
+		this.queue = queue;
+	}
 
 }
